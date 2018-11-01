afacf779684a5c68da9767e07039951c9c852d4b
camron <camronmoradi@gmail.com>
2018-11-01 15:49:32 +0100

    [WIP] create cart method

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,14 +1,46 @@
 class Checkout
-
+attr_accesor :cart, :product
+   
     def initialize
+        @cart = []  
+        @product = set_products
+    end
+
+    def cart()
         
     end
 
     def scan(item)
         case item
-            when 'Lavender heart'
-                    9.25
+        when
         end
+        @cart << item
     end
+
+
+    # def find_item
+    # end
     
+    private
+
+    def set_products
+        [
+          {
+            product: 1,
+            name: 'Lavender heart',
+            price: 9.25
+          },
+          {
+            product: 2,
+            name: 'Personalised cufflinks',
+            price: 45.00
+          },
+          {
+            product: 3,
+            name: 'Kids T-shirt',
+            price: 19.95
+          }
+        ]
+      end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,4 +7,8 @@ describe Checkout do
         expect(Checkout.new.scan('Lavender heart')).to eq 9.25
     end
 
+    # it 'calculate total basket amount' do
+    #     expect(subject.basket(total)).to eq 74.2
+    # end
+
 end
